72143d2034f9e540908c49c7a57b0e7cf537da68
unnamed <>
2020-02-11 15:28:20 +0100

    Restrict Sending Error Message
    Do not send error message
    when status code 500 and env production

--- src/server/middleware/errorMiddleware.ts
+++ src/server/middleware/errorMiddleware.ts
@@ -1,10 +1,18 @@
-import { Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import HttpException from '../utils/HttpException';
 import logger from '../../logger';
 
-export default function errorMiddleware(err: HttpException, _req: Request, res: Response, _next: NextFunction) {
+export default function errorMiddleware(
+	err: HttpException,
+	_req: Request,
+	res: Response,
+	_next: NextFunction
+) {
 	const status = err.statusCode || res.statusCode || 500;
-	const message = err.message;
+	const message =
+		process.env.NODE_ENV === 'production' && status === 500
+			? 'Something went wrong'
+			: err.message;
 	const data = err.data || {};
 
 	const resObj = {
@@ -14,10 +22,9 @@ export default function errorMiddleware(err: HttpException, _req: Request, res: 
 	};
 	if (status === 500) {
 		logger.error('%o', resObj);
-	}
-	else {
+	} else {
 		logger.warn('%o', resObj);
 	}
 
 	res.status(status).send(resObj);
-};
+}
