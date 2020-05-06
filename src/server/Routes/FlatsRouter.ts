601ae10639371fbe8da29e2df54a4c3c40f7fa14
unnamed <>
2020-05-06 10:19:14 +0200

    Rename file

--- src/server/Routes/FlatsRouter.ts
+++ src/server/Routes/FlatsRouter.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { getFlats, create, deleteFlat } from '../controllers/FlatsController';
 import { addMembers, deleteMembers, getMembers } from '../controllers/FlatMembersController';
-import tasksRouter from './TasksRouter';
+import flatTasksRouter from './FlatTasksRouter';
 const router = Router({ mergeParams: true });
 
 router.get('/', getFlats);
 router.post('/', create);
 router.delete('/:id', deleteFlat);
 
-router.use('/:flatId/tasks', tasksRouter);
+router.use('/:flatId/tasks', flatTasksRouter);
 
 router.get('/:flatId/members', getMembers);
 router.patch('/:flatId/members', addMembers);
