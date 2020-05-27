eb9c66aa92f0eafe6e047f0d5f1faa59b60bfc3e
unnamed <>
2020-05-27 13:44:31 +0200

    List logger levels

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,19 +1,28 @@
 import { createLogger, transports, format } from 'winston';
 import path from 'path';
-
 import { consoleTransportFormat } from './loggerHelpers';
+
+export type LoggerLevel =
+	| 'error'
+	| 'warn'
+	| 'info'
+	| 'http'
+	| 'verbose'
+	| 'debug'
+	| 'silly';
+
 const logger = createLogger({
 	level: process.env.TRACE_LEVEL,
 	format: format.combine(
 		format.timestamp({
-			format: 'YYYY-MM-DD HH:mm:ss'
+			format: 'YYYY-MM-DD HH:mm:ss',
 		}),
 		format.errors({ stack: true }),
 		format.splat(),
 		format.json({ space: 4 }),
 		format.ms(),
 		format.label({ label: '[FTS 2020-server]' }),
-		format.printf(info => {
+		format.printf((info) => {
 			return (
 				`${info.label} ${info.timestamp} (${info.ms}) ${info.level}: ${info.message}` +
 				(info.stack ? '\nStack: ' + info.stack : '')
@@ -28,7 +37,7 @@ const logger = createLogger({
 			handleExceptions: false,
 			dirname: path.join(__dirname, './../../logs'),
 			maxsize: 5 * 1024,
-			maxFiles: 10
+			maxFiles: 10,
 		}),
 		new transports.File({
 			level: 'error',
@@ -36,21 +45,25 @@ const logger = createLogger({
 			handleExceptions: false,
 			dirname: path.join(__dirname, './../../logs/errors'),
 			maxsize: 5 * 1024,
-			maxFiles: 10
-		})
+			maxFiles: 10,
+		}),
 	],
-	exitOnError: false
+	exitOnError: false,
 });
 
 if (process.env.NODE_ENV !== 'production') {
 	logger.add(
 		new transports.Console({
 			format: consoleTransportFormat,
-			handleExceptions: false
+			handleExceptions: false,
 		})
 	);
 }
-logger.log(process.env.TRACE_LEVEL!, 'logger: TRACE_LEVEL: %s', process.env.TRACE_LEVEL);
+logger.log(
+	process.env.TRACE_LEVEL!,
+	'logger: TRACE_LEVEL: %s',
+	process.env.TRACE_LEVEL
+);
 
 export default logger;
 module.exports = logger;
