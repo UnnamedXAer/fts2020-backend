77b90adbdb756191db7e52b43d3b2c3160c5d555
unnamed <>
2020-05-29 11:55:15 +0200

    Change POST to PATCH method for inv update

--- src/server/Routes/InvitationsRouter.ts
+++ src/server/Routes/InvitationsRouter.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 import { updateFlatInvitationStatus } from '../controllers/FlatMembersController';
 const router = Router({ mergeParams: true });
 
-router.post('/:id', updateFlatInvitationStatus);
+router.patch('/:id', updateFlatInvitationStatus);
 
 export default router;
