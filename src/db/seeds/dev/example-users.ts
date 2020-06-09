4f21a2364870ef42ae0519721aafc40f4aa067bb
unnamed <>
2020-06-09 15:11:36 +0200

    Update seed file

--- src/db/seeds/dev/example-users.ts
+++ src/db/seeds/dev/example-users.ts
@@ -2,21 +2,14 @@ import Knex from 'knex';
 import bcrypt from 'bcrypt';
 
 export async function seed(knex: Knex): Promise<any> {
-    const hashPassword = await bcrypt.hash(
-        'Pwd12345',
-        await bcrypt.genSalt(10)
-    );
+	const hashPassword = await bcrypt.hash('qwe123', await bcrypt.genSalt(10));
 
-    const avatarUrl =
-        'https://store-images.s-microsoft.com/image/apps.46874.37ea8e43-6d69-48a4-a8da-47580dfe55b5.cf90a1b2-49f6-4c7d-9984-f3d287b579c2.4adf6cdc-92cc-4260-bdb3-cca09c2ea124.png';
-
-    // Deletes ALL existing entries
-    return Promise.all<{
+	return Promise.all<{
 		command: string;
 		rowCount: number;
 	}>([
 		knex('appUser')
-			.del()
+		.del()  // Deletes ALL existing entries !!!
 			.then(() => {
 				// Inserts seed entries
 				return knex('appUser').insert([
@@ -25,47 +18,60 @@ export async function seed(knex: Knex): Promise<any> {
 						userName: 'Long John Silver',
 						provider: 'local',
 						password: hashPassword,
-						avatarUrl,
-						active: true
+						avatarUrl:
+							'http://pngimg.com/uploads/pokemon/pokemon_PNG161.png',
+						active: true,
 					},
 					{
 						emailAddress: 'rkl2@o2.pl',
 						userName: 'Kamil',
 						provider: 'local',
 						password: hashPassword,
-						avatarUrl,
-						active: true
+						avatarUrl:
+							'http://pngimg.com/uploads/pokemon/pokemon_PNG160.png',
+						active: true,
 					},
 					{
 						emailAddress: 'no@test.com',
 						userName: 'Ann',
 						provider: 'local',
 						password: hashPassword,
-						avatarUrl,
-						active: false
+						avatarUrl:
+							'http://pngimg.com/uploads/pokemon/pokemon_PNG153.png',
+						active: false,
 					},
 					{
 						emailAddress: 'dean@test.com',
 						userName: 'Dean W',
 						provider: 'local',
 						password: hashPassword,
-						avatarUrl,
-						active: true
-					}
+						avatarUrl:
+							'http://pngimg.com/uploads/pokemon/pokemon_PNG149.png',
+						active: true,
+					},
+					{
+						emailAddress: 'unnamedxaer@hotmail.com',
+						userName: 'UnnamedXAer',
+						provider: 'local',
+						password: hashPassword,
+						avatarUrl:
+							'http://pngimg.com/uploads/pokemon/pokemon_PNG34.png',
+						active: true,
+					},
 				]);
 			}),
 		knex('logs').insert({
 			txt: 'Run seed: example-users',
-			source: 'seed/dev/'
-		})
+			source: 'seed/dev/',
+		}),
 	])
-		.then(res => {
+		.then((res) => {
 			console.log('Done seeding: example-users.');
-			res.forEach(x =>
+			res.forEach((x) =>
 				console.log(`command: ${x.command}, rowCount: ${x.rowCount}`)
 			);
 		})
-		.catch(err =>
+		.catch((err) =>
 			console.error(
 				'Error in seeding: example-users\n',
 				err && err.detail ? err.detail : err
