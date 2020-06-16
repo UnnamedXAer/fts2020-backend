fdcf901975537b068326ef4925cd96f5f63fed15
unnamed <>
2020-06-16 14:01:45 +0200

    Add property to  401 response when user not logged

--- src/server/middleware/ensureAuthenticated.ts
+++ src/server/middleware/ensureAuthenticated.ts
@@ -6,7 +6,9 @@ const ensureAuthenticated: RequestHandler = (req, _, next) => {
 	if (req.isAuthenticated()) {
 		return next();
 	}
-	return next(new HttpException(401, 'Unauthorized access.'));
+	return next(new HttpException(401, 'Unauthorized access.', {
+		no_user_logged: true
+	}));
 };
 
 export default ensureAuthenticated;
