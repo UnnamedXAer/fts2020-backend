48950631e49a1945d4197549405f0be909e8dd83
unnamed <>
2020-06-18 13:21:29 +0200

    Add logic to generate periods for DAY unit

--- src/server/utils/TaskTimePeriod.ts
+++ src/server/utils/TaskTimePeriod.ts
@@ -7,19 +7,16 @@ export function updateDates(
 	index: number
 ) {
 	// let offset = 0;
-	const dayInMs = 1000 * 60 * 60 * 24;
 
 	switch (unit) {
 		// case TaskPeriodUnit.HOUR:
 		// 	offset = 1000 * 60 * 60 * value;
 		// 	// endDate = new Date(startDate.getTime() + offset);
 		// 	break;
-		// case TaskPeriodUnit.DAY:
-		// 	offset = dayInMs * value;
-		// 	// endDate = new Date(startDate.getTime() + offset);
-		// 	break;
+		case TaskPeriodUnit.DAY:
+			return calculateNextDatesForDay(startDate, value, index);
 		case TaskPeriodUnit.WEEK:
-			return calculateNextDatesForWeek(startDate, dayInMs, value, index);
+			return calculateNextDatesForWeek(startDate, value, index);
 		// case TaskPeriodUnit.MONTH:
 		// 	// endDate = new Date(
 		// 	// 	new Date(startDate).setMonth(startDate.getMonth() + value)
@@ -36,16 +33,15 @@ export function updateDates(
 
 function calculateNextDatesForWeek(
 	startDate: Date,
-	dayInMs: number,
 	value: number,
 	index: number
 ) {
+	const dayInMs = 1000 * 60 * 60 * 24;
 	const startDateOffset = dayInMs * (index > 0 ? 1 : 0);
-	let endDateOffset:number
+	let endDateOffset: number;
 	if (value === 1) {
 		endDateOffset = dayInMs * 6 * value;
-	}
-	else {
+	} else {
 		endDateOffset = dayInMs * 7 * value - dayInMs;
 	}
 
@@ -54,6 +50,26 @@ function calculateNextDatesForWeek(
 
 	return {
 		currentStartDate,
-		currentEndDate
+		currentEndDate,
+	};
+}
+
+function calculateNextDatesForDay(
+	startDate: Date,
+	value: number,
+	index: number
+) {
+	const dayInMs = 1000 * 60 * 60 * 24;
+
+	const startDateOffset = dayInMs * (index > 0 ? 1 : 0);
+	let endDateOffset: number;
+	endDateOffset = dayInMs * (value - 1);
+
+	const currentStartDate = new Date(startDate.getTime() + startDateOffset);
+	const currentEndDate = new Date(currentStartDate.getTime() + endDateOffset);
+
+	return {
+		currentStartDate,
+		currentEndDate,
 	};
 }
